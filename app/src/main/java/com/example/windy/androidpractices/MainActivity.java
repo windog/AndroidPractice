6fbb7baf34caf2218113fe2af946574418daf553
windog <windog.dev@gmail.com>
2016-07-31 16:34:08 +0800

    AsyncTask item layout

--- app/src/main/java/com/example/windy/androidpractices/MainActivity.java
+++ app/src/main/java/com/example/windy/androidpractices/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.windy.androidpractices;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.orhanobut.logger.Logger;
 
@@ -16,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = (Button) findViewById(R.id.btn_toActivity2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
         SketchView sketchView = (SketchView) findViewById(R.id.sketch_view);
         sketchView.startAnimation();
