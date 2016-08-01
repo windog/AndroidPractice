5a4e04b1b86b5ef4ef4d039a1352d5729963dcd0
windog <windog.dev@gmail.com>
2016-08-01 11:08:43 +0800

    Test AsyncTask

--- app/src/main/java/com/example/windy/androidpractices/SecondActivity.java
+++ app/src/main/java/com/example/windy/androidpractices/SecondActivity.java
@@ -1,18 +1,128 @@
 package com.example.windy.androidpractices;
 
 import android.app.Activity;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by windog on 2016/7/22.
  */
 public class SecondActivity extends Activity {
+
+    private ListView mListView;
+    //慕课网提供的后台数据
+    private static final String URL = "http://www.imooc.com/api/teacher?type=4&num=30";
+    private static final String LOG_TAG = SecondActivity.class.getClass().getSimpleName();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Button button2 = (Button) findViewById(R.id.btn2);
+        mListView = (ListView) findViewById(R.id.lv_main);
+        new NewsAsyncTask().execute(URL);
+    }
+
+    private List<NewsBean> getJsonData(String url) {
+        List<NewsBean> newsBeanList = new ArrayList<>();
+        try {
+            // new URL(url).openStream() 这一句和 url.openConnection().getInputStream() 相同
+            // 可根据 URL 直接联网获取网络数据，返回类型为 InputStream
+            String jsonString = readStream(new URL(url).openStream());
+            Log.d(LOG_TAG, jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newsBeanList;
+    }
+
+    private String readStream(InputStream inputStream) {
+        InputStreamReader isr;
+        String result = "";
+        try {
+            String line = "";
+            // 字节流转成字符流
+            isr = new InputStreamReader(inputStream, "utf-8");
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            while ((line = bufferedReader.readLine()) != null) {
+                result = result + line;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Second Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.windy.androidpractices/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Second Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.windy.androidpractices/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    // 返回的是 NewsBean 的集合
+    class NewsAsyncTask extends AsyncTask<String, Void, List<NewsBean>> {
+
+        @Override
+        protected List<NewsBean> doInBackground(String... params) {
+            return getJsonData(params[0]);
+        }
     }
 }
